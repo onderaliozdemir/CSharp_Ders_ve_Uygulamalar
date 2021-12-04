4e0efb6452772dc0bf02fdea1c2c6cf46ba96933
onderaliozdemir <onderozdemir184@gmail.com>
2021-12-05 00:10:03 +0300

    shorcut güncelleme

--- Lesson & Practices/2 - Interfaces Gerçek Hayat Kullanımı/Program.cs
+++ Lesson & Practices/2 - Interfaces Gerçek Hayat Kullanımı/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿/*
+ * Created on Sun Dec 05 2021
+ * Author:  Önder Ali ÖZDEMİR
+ * onderozdemir184@gmail.com
+ */
 
+
+using System;
 namespace _2___Interfaces_Gerçek_Hayat_Kullanımı
 {
     class Program
