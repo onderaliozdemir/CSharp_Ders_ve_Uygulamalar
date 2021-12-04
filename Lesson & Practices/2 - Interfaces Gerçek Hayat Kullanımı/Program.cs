1bde6dd965d7d75a8dc2c8219d9b1a5f1b6daace
onderaliozdemir <onderozdemir184@gmail.com>
2021-12-04 23:50:23 +0300

    İnterface gerçek hayat kullanımı

---
+++ Lesson & Practices/2 - Interfaces Gerçek Hayat Kullanımı/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _2___Interfaces_Gerçek_Hayat_Kullanımı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+    interface IPerson
+    { 
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public string Email { get; set; }
+
+    }
+
+    class Student : IPerson
+    { 
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager{
+        public void Add(IPerson person){
+            System.Console.WriteLine(person.FirstName);
+        }
+    }
+
+    class Worker:IPerson{
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public string WorkingTime { get; set; }
+    }
+}
