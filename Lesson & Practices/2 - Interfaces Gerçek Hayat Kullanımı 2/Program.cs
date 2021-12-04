5f55b47e7467e802bd9a033b42d85992d80edccf
onderaliozdemir <onderozdemir184@gmail.com>
2021-12-05 00:42:19 +0300

    interface 2

--- Lesson & Practices/2 - Interfaces Gerçek Hayat Kullanımı 2/Program.cs
+++ Lesson & Practices/2 - Interfaces Gerçek Hayat Kullanımı 2/Program.cs
@@ -12,7 +12,66 @@ namespace _2___Interfaces_Gerçek_Hayat_Kullanımı_2
     {
         static void Main(string[] args)
         {
-            
+            // Poliformizm ?
+            // IDatabase nesnesini SqlServer ve Mongo olarak implemente ettik. Çok Biçimlilik kazandırdık.
+            // SQL VE MONGO YA AYNI ANDA INTERFACE KULLANARAK İŞLEM YAPTIRMAK.
+            IDatabase[] databases=new IDatabase[]{
+                new SqlServerDAL(),
+                new MongoDAL()
+            };
+            foreach (IDatabase database in databases)
+            {
+                database.Add();
+            }
+
+        }
+    }
+    public interface IDatabase{
+
+        void Add();
+        void Delete();
+        void Update();
+    }
+
+    public class SqlServerDAL : IDatabase
+    {
+        public void Add()
+        {
+            System.Console.WriteLine("Sql'e eklendi");
+        }
+
+        public void Delete()
+        {
+            System.Console.WriteLine("Sql' den silindi.");
+        }
+
+        public void Update()
+        {
+            System.Console.WriteLine("Sqlden update edildi.");
+        }
+    }
+    public class MongoDAL : IDatabase
+    {
+        public void Add()
+        {
+            System.Console.WriteLine("Mongo ya eklendi");
+        }
+
+        public void Delete()
+        {
+            System.Console.WriteLine("Mongo dan silindi.");
+        }
+
+        public void Update()
+        {
+            System.Console.WriteLine("Mongo ya update edildi.");
+        }
+    }
+
+    public class DatabaseManager{
+
+        public void Add(IDatabase database){
+            database.Add();
         }
     }
 }
