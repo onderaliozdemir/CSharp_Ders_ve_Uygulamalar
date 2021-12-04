d4ec013519c3b6f21c55e611e5ba47f86a85be4d
onderaliozdemir <onderozdemir184@gmail.com>
2021-12-04 21:49:22 +0300

    Interface Genel Bilgiler

---
+++ Lesson & Practices/2 - Interfaces Neden Kullanılır/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2___Interfaces_Neden_Kullanılır
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson & Practices/2 - Interfaces/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _2___Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* #region INTERFACE NEDİR? */
+            /*
+                
+                Temel operasyon temel nesne oluşturup tüm nesneleri ondan türetmek.
+                Örnek Person, Customer ve Student ile örneklendirelim.
+                NOT : Class gibi tanımlanır. Class yerine interface yazılır.
+            */
+            /* #endregion */
+        }
+    }
+    interface IPerson
+    { 
+
+        /* #region INTERFACE NOTLARI: */
+        /*
+        Soyut Nesne (Abstract) : Tek başına hiçbir şey ifade etmez.
+
+        Müşteri de öğrenci de bir insan dır. (Person) IPerson da tanımlanmış herşeyi
+        hem Customer da hemde Student da kullanabilirim.
+        
+             - Interface içinde sadece imza bulunur.
+             - public gibi erişim belirleyiciler olmaz. 
+             - class'a implemente edilmiş tüm property ve methodlar class'da kullanılmak zorundadır.
+             - imzalar dışında classa özel ek özellik veya method eklenebilir.
+        */
+        /* #endregion */
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    class Customer : IPerson // Somut Nesne // -> : ile interface'i bir class'a implemente ediyoruz.
+    {
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public string Email { get; set; } // Student class'ına kıyasla farklı bir özellik tanımladım.
+
+    }
+
+    class Student : IPerson // Somut Nesne
+    { 
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public int Departmant { get; set; } // Student'a özel property
+    }
+}
