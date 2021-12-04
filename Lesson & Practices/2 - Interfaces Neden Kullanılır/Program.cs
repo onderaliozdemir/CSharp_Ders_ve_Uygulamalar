bf99d3261c86410d1ab978e08d5f72a2555e546b
onderaliozdemir <onderozdemir184@gmail.com>
2021-12-04 22:17:42 +0300

    İnterface neden kullanılır?

--- Lesson & Practices/2 - Interfaces Neden Kullanılır/Program.cs
+++ Lesson & Practices/2 - Interfaces Neden Kullanılır/Program.cs
@@ -6,7 +6,78 @@ namespace _2___Interfaces_Neden_Kullanılır
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region !! INTERFACE' I EKSİK VEYA HATALI KULLANMA VE GEREKLİLİK SEBEBİ 
+            /*
+            PersonManager manager=new PersonManager();
+            manager.Add(new Customer{
+                    Id=1,
+                    FirstName="Önder",
+                    LastName="Özdemir",
+                    Email="onderozdemir184@gmail.com"
+            });
+
+            Student student=new Student{
+                Id=1,
+                FirstName="Veli",
+                LastName="Kan",
+                Departmant="Bilgisayar Prog."
+            };
+            //manager.Add(student); // PROBLEM BU. Customer isteyen CustomerManager'a Student Göndermek istiyorum ancak Student kabul etmiyor.
+            */
+            #endregion
+
+            PersonManager manager=new PersonManager();
+            manager.Add(new Customer{
+                    Id=1,
+                    FirstName="Ali",
+                    LastName="Özdemir",
+                    Email="o@o.com"
+            });
+
+            manager.Add(new Student{
+                Id=2,
+                FirstName="Önder",
+                LastName="Özdemir",
+                Departmant="Bilgisayar Müh."
+            });
+        }
+    }
+    interface IPerson
+    { 
+
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public string Email { get; set; }
+
+    }
+
+    class Student : IPerson
+    { 
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get ; set; }
+        public string Departmant { get; set; }
+    }
+
+    /*
+    class PersonManager{ // İş katmanı sınıflarımız için kullanılır.
+
+        public void Add(Customer customer){
+            System.Console.WriteLine(customer.FirstName);
+        }
+    }
+    */
+    class PersonManager{
+        public void Add(IPerson person){
+            System.Console.WriteLine(person.FirstName);
         }
     }
 }
